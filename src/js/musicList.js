0f8c79f96221ae7bee24c1a4a451bc598415cb41
wwj <664027523@qq.com>
2018-08-28 11:18:23 +0800

    添加读取功能，新建歌曲和列表 active 高亮

--- src/js/musicList.js
+++ src/js/musicList.js
@@ -5,7 +5,7 @@
     <ul>
         <li>歌曲1</li>
         <li>歌曲2</li>
-        <li class="active">歌曲3</li>
+        <li>歌曲3</li>
         <li>歌曲4</li>
         <li>歌曲5</li>
         <li>歌曲6</li>
@@ -13,42 +13,83 @@
     </ul>
     `,
     render(data) {
-      let {songs}=data
-      let list=[]
-      $(this.el).find('ul').empty()
-      songs.map((songs)=>{
-        list.push(songs.songName)
-      })
-      list.map((songName)=>{
-        $(this.el).find('ul').append(`<li>${songName}</li>`)
-      })
-      
+      let { songs } = data;
+      $(this.el)
+        .find("ul")
+        .empty();
+
+      let liList = songs.map(songs => {
+        return $("<li></li>")
+          .html(songs.songName)
+          .attr("song-id", songs.id);//添加歌曲 id 属性
+      });
+      console.log(liList);
+      liList.map(domLi => {
+        $(this.el)
+          .find("ul")
+          .append(domLi);
+      });
+
       //$(this.el).html(this.template);
+    },
+    removeActive() {
+      $(this.el)
+        .find(".active")
+        .removeClass("active");
+    },
+    addActive(item) {
+      $(item).addClass("active");
     }
   };
-  let model={
-    data:{
-      songs:[]
+  let model = {
+    data: {
+      songs: []
+    },
+    find() {
+      var query = new AV.Query("Song");
+      return query.find().then(songs => {
+        songs.map(song => {
+          this.data.songs.push({ id: song.id, ...song.attributes });
+        });
+        return songs;
+      });
     }
-  }
-  let control={
-      init(view,model){
-        this.view=view
-        this.model=model
-        this.view.render(this.model.data)
-        window.eventHub.on('upload',()=>{
-          this.removeActive()
-        })
-        window.eventHub.on('creatSongMessage',(data)=>{
-          console.log(JSON.stringify(this.model.data.songs) )
-          this.model.data.songs.push(data)
-          console.log(JSON.stringify(this.model.data.songs) )
-          this.view.render(this.model.data)
-        })
-      },
-      removeActive(){
-        $(this.view.el).find('.active').removeClass('active')
-      }
-  }
-  control.init(view,model)
+  };
+  let control = {
+    init(view, model) {
+      this.view = view;
+      this.model = model;
+      this.view.render(this.model.data);
+      this.getAllSongs();
+      this.bindEventHub();
+      this.bindEvents();
+    },
+    bindEvents() {
+      $(this.view.el).on("click", "li", e => {
+        this.view.removeActive();
+        this.view.addActive(e.currentTarget);
+        console.log(e.currentTarget);
+        let songId=e.currentTarget.getAttribute('song-id')
+        window.eventHub.emit('listActive',{'song-id':songId})
+      });
+    },
+    bindEventHub() {
+      window.eventHub.on("upload", () => {
+        this.view.removeActive();
+      });
+      window.eventHub.on("creatSongMessage", data => {
+        this.model.data.songs.push(data);
+        this.view.render(this.model.data);
+      });
+      window.eventHub.on('newSong-active',()=>{
+        this.view.removeActive()
+      })
+    },
+    getAllSongs() {
+      this.model.find().then(x => {
+        this.view.render(this.model.data);
+      });
+    }
+  };
+  control.init(view, model);
 }
--- src/js/newSong.js
+++ src/js/newSong.js
@@ -6,7 +6,13 @@
       `,
       render(data) {
         $(this.el).html(this.template);
-      }
+      },
+      active(){
+        $(this.el).addClass('active')
+    },
+    removeActive(){
+      $(this.el).removeClass('active')
+  }
     };
     let model={}
     let control={
@@ -14,13 +20,22 @@
           this.view=view
           this.model=model
           this.view.render(this.model.data)
+          this.bindEvents()
           window.eventHub.on('upload',(data)=>{
-            this.active()
+            this.view.active()
+          })
+          window.eventHub.on('listActive',(songId)=>{
+            console.log(songId)
+            this.view.removeActive()
           })
         },
-        active(){
-            $(this.view.el).addClass('active')
-        }
+        bindEvents(){
+          $(this.view.el).on('click',()=>{
+            this.view.active()
+            window.eventHub.emit('newSong-active')
+          })
+        },
+
     }
     control.init(view,model)
   }
