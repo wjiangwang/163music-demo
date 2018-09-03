3f34155a079a2e90d4fd87e2840a328a16a96994
wwj <664027523@qq.com>
2018-09-03 13:30:46 +0800

    用户端获取歌曲列表 （标签切换 、最新音乐 MVC）

---
+++ src/js/index/navbar.js
@@ -0,0 +1,29 @@
+{
+    let view={
+        el:'#navbar'
+    }
+    let model={}
+    let controller={
+        init(view,model){
+            this.view=view
+            this.model=model
+            this.bindEvents()
+        },
+        bindEvents(){
+            $(this.view.el).on("click", "li", function(e) {
+                $(e.currentTarget)
+                  .addClass("active")//点击时的红色底边
+                  .siblings()
+                  .removeClass("active");
+                  
+                let className = $(e.currentTarget)[0].id;
+                console.log($(e.currentTarget)[0])//标贴切换
+                $(".content").removeClass("active");
+                $(`.${className}`).addClass("active");
+              });
+        }
+
+        
+    }
+    controller.init(view,model)
+}
